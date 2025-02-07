9a8e1b95d8d921899609900459a13b3623f9987e
Leo Chung <gewalalb@gmail.com>
2025-02-07 11:00:02 +0800

    unix/lvgl: support LVGL

--- extmod/extmod.mk
+++ extmod/extmod.mk
@@ -619,3 +619,46 @@ $(addprefix $(BUILD)/, $(SRC_OPENAMP_C:.c=.o)): $(BUILD)/openamp/metal/config.h
 SRC_THIRDPARTY_C += $(SRC_LIBMETAL_C) $(SRC_OPENAMP_C)
 
 endif # MICROPY_PY_OPENAMP
+
+ifeq ($(MICROPY_PY_LVGL),1)
+# LVGL build rules
+LVGL_BINDING_DIR = $(TOP)/lib/lv_bindings
+LVGL_DIR = $(LVGL_BINDING_DIR)/lvgl
+LVGL_GENERIC_DRV_DIR = $(LVGL_BINDING_DIR)/driver/generic
+INC += -I$(LVGL_BINDING_DIR)
+ALL_LVGL_SRC = $(shell find $(LVGL_DIR) -type f -name '*.h') $(LVGL_BINDING_DIR)/lv_conf.h
+LVGL_ALL_H = $(BUILD)/lvgl/lvgl_all.h
+LVGL_ALL_JSON = $(BUILD)/lvgl/lvgl_all.json
+LVGL_PP = $(BUILD)/lvgl/lvgl.pp.c
+LVGL_MPY = $(BUILD)/lvgl/lv_mpy.c
+LVGL_MPY_METADATA = $(BUILD)/lvgl/lv_mpy.json
+CFLAGS_EXTMOD += $(LV_CFLAGS) 
+
+ifneq (,$(wildcard $(LVGL_DIR)/scripts/gen_json/gen_json.py))
+$(LVGL_ALL_JSON): $(ALL_LVGL_SRC) $(LVGL_DIR)/scripts/gen_json/gen_json.py
+	$(ECHO) "LVGL-JSON-GEN $@"
+	$(Q)mkdir -p $(dir $@)
+	$(ECHO) "#include \"$(LVGL_DIR)/lvgl.h\"\n#include \"$(LVGL_DIR)/src/lvgl_private.h\"" > $(LVGL_ALL_H)
+	$(Q)$(PYTHON) $(LVGL_DIR)/scripts/gen_json/gen_json.py --target-header $(LVGL_ALL_H) > $(LVGL_ALL_JSON)
+else
+$(LVGL_ALL_JSON):
+	$(ECHO) "LVGL-JSON-GEN $@"
+	$(Q)mkdir -p $(dir $@)
+	$(ECHO) "{}" > $(LVGL_ALL_JSON)
+endif
+
+$(LVGL_MPY): $(ALL_LVGL_SRC) $(LVGL_BINDING_DIR)/gen/gen_mpy.py $(LVGL_ALL_JSON)
+	$(ECHO) "LVGL-GEN $@"
+	$(Q)mkdir -p $(dir $@)
+	$(Q)$(CPP) $(CFLAGS_EXTMOD) -DPYCPARSER -x c -I $(LVGL_BINDING_DIR)/pycparser/utils/fake_libc_include $(INC) $(LVGL_DIR)/lvgl.h > $(LVGL_PP)
+	$(Q)$(PYTHON) $(LVGL_BINDING_DIR)/gen/gen_mpy.py -M lvgl -MP lv -MD $(LVGL_MPY_METADATA) -E $(LVGL_PP) -J $(LVGL_ALL_JSON) $(LVGL_DIR)/lvgl.h > $@
+
+.PHONY: LVGL_MPY
+LVGL_ALL_JSON: $(LVGL_ALL_JSON)
+LVGL_MPY: $(LVGL_MPY)
+
+CFLAGS_EXTMOD += -Wno-unused-function
+SRC_THIRDPARTY_C += $(subst $(TOP)/,,$(shell find $(LVGL_DIR)/src $(LVGL_DIR)/examples $(LVGL_GENERIC_DRV_DIR) -type f -name "*.c"))
+SRC_EXTMOD_C += $(LVGL_MPY)
+
+endif # MICROPY_PY_LVGL
